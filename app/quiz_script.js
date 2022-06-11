795f62003b2470fce8081d543ca95d9253caa585
m-chan <mantingchan2@gmail.com>
2022-06-11 18:16:23 +0100

    starting on quiz functionality and questions

---
+++ app/quiz_script.js
@@ -0,0 +1,19 @@
+
+const questions  = [
+    {
+        question: 'What field are you interested in?',
+        answers: []
+    },
+    {
+        question: 'What career roles have you held?',
+        answers: []
+    }
+]
+
+let questionText = document.getElementById('questions')
+
+questionText.innerText = questions[0].question
+
+function next() {
+
+}
